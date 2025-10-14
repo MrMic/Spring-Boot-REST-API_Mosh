8dd128f40ecb231afb0a09d4c3b32d2d561dbd4e
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-10-14 15:28:57 +0200

    feat: enhance product and user management features - Ex. Build a CRUD API -
    - Add category-based product creation and update endpoints
    - Implement password change endpoint for user accounts
    - Introduce ChangePasswordRequest DTO for handling password updates
    - Extend ProductMapper with update support

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -1,16 +1,17 @@
 package com.codewithmosh.store.controllers;
 
+import com.codewithmosh.store.dtos.ChangePasswordRequest;
 import com.codewithmosh.store.dtos.RegisterUserRequest;
 import com.codewithmosh.store.dtos.UpdateUserRequest;
 import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.mappers.UserMapper;
 import com.codewithmosh.store.repositories.UserRepository;
+import java.util.Set;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
-
-import java.util.Set;
 
 @RestController
 @RequestMapping("/users")
@@ -25,9 +26,10 @@ public class UserController {
 
   @GetMapping
   public Iterable<UserDto> getAllUsers(
-//      @RequestHeader( required = false, name = "x-auth-token") String authToken,
+      // @RequestHeader( required = false, name = "x-auth-token") String authToken,
       @RequestParam(required = false, defaultValue = "", name = "sort") String sortBy) {
-//    System.out.println("Auth Token: " + authToken); // For demonstration purposes only
+    // System.out.println("Auth Token: " + authToken); // For demonstration purposes
+    // only
 
     if (!Set.of("name", "email").contains(sortBy)) sortBy = "name"; // validate sort parameter
 
@@ -46,8 +48,7 @@ public class UserController {
 
   @PostMapping
   public ResponseEntity<UserDto> createUser(
-      @RequestBody RegisterUserRequest request,
-      UriComponentsBuilder uriBuilder) {
+      @RequestBody RegisterUserRequest request, UriComponentsBuilder uriBuilder) {
     var user = userMapper.toEntity(request);
     userRepository.save(user);
 
@@ -58,8 +59,7 @@ public class UserController {
 
   @PutMapping("/{id}")
   public ResponseEntity<UserDto> updateUser(
-      @PathVariable(name = "id") Long id,
-      @RequestBody UpdateUserRequest request) {
+      @PathVariable(name = "id") Long id, @RequestBody UpdateUserRequest request) {
     var user = userRepository.findById(id).orElse(null);
     if (user == null) {
       return ResponseEntity.notFound().build();
@@ -72,13 +72,33 @@ public class UserController {
   }
 
   @DeleteMapping("/{id}")
-  public  ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+  public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
     var user = userRepository.findById(id).orElse(null);
     if (user == null) {
       return ResponseEntity.notFound().build();
     }
 
     userRepository.deleteById(id);
+    return ResponseEntity.noContent().build();
+  }
+
+  @PostMapping("/{id}/change-password")
+  public ResponseEntity<Void> changePassword(
+      @PathVariable Long id, /*, @RequestBody ChangePasswordRequest request*/
+      @RequestBody ChangePasswordRequest request
+      ) {
+    var user = userRepository.findById(id).orElse(null);
+    if (user == null) {
+      return ResponseEntity.notFound().build();
+    }
+
+    if (!user.getPassword().equals(request.getOldPassword())) {
+      return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+    }
+
+    user.setPassword(request.getNewPassword());
+    userRepository.save(user);
+
     return ResponseEntity.noContent().build();
   }
 }
