bc94c857587f0431f3bcb6f706dbcbe3a24031c0
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-10-18 14:59:06 +0200

    feat: add email validation and duplicate checks for user registration - 34 + 35: Custom Validation & Business Rules Validation
    - Introduce @Lowercase validation annotation
    - Enforce email to be lowercase and max 255 characters
    - Add duplicate email check in UserRepository
    - Update UserController to handle registration errors

---
+++ src/main/java/com/codewithmosh/store/validation/LowercaseValidator.java
@@ -0,0 +1,14 @@
+package com.codewithmosh.store.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class LowercaseValidator implements ConstraintValidator<Lowercase, String> {
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return true; // Consider using @NotNull for null checks
+        }
+        return value.equals(value.toLowerCase());
+    }
+}
