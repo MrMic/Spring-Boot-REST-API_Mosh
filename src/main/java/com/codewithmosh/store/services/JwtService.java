0bc9fce2093491f3d295bee3e2b330c5e737ad51
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-11-08 13:11:01 +0100

    feat: integrate JWT-based authentication filter - 59/ Custom Filter to validate JWTs
    - Add JwtAuthenticationFilter for processing JWT tokens
    - Register JWT filter in SecurityConfig with filter chain
    - Enable token validation and user extraction in JwtService
    - Add utility methods for claims and email extraction from tokens

--- src/main/java/com/codewithmosh/store/services/JwtService.java
+++ src/main/java/com/codewithmosh/store/services/JwtService.java
@@ -1,18 +1,19 @@
 package com.codewithmosh.store.services;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import java.util.Date;
 
 @Service
 public class JwtService {
   @Value("${spring.jwt.secret}")
   private String secret;
 
+  // ──────────────────────────────────────────────────────────────────────
   public String generateToken(String email) {
     final long tokenExpiration = 86400; // 1 day in seconds
 
@@ -24,18 +25,28 @@ public class JwtService {
         .compact();
   }
 
+  // ──────────────────────────────────────────────────────────────────────
   public boolean validateToken(String token) {
- try {
-      var claims = Jwts.parser()
-      .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
-      .build()
-      .parseSignedClaims(token)
-      .getPayload();
+    try {
+      var claims = getClaims(token);
 
       return claims.getExpiration().after(new Date());
-  } catch (JwtException ex) {
+    } catch (JwtException ex) {
       return false;
     }
   }
 
+  // ──────────────────────────────────────────────────────────────────────
+  private Claims getClaims(String token) {
+    return Jwts.parser()
+        .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
+        .build()
+        .parseSignedClaims(token)
+        .getPayload();
+  }
+
+  // ──────────────────────────────────────────────────────────────────────
+  public String getEmailFromToken(String token) {
+    return getClaims(token).getSubject();
+  }
 }
