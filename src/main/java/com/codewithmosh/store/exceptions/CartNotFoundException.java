a697b5450aea3965c672b70a66c41a0f57678405
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-11-25 10:01:48 +0100

    refactor: simplify checkout process and add exceptions - 78/ Extracting a Service
    - Replace manual error handling with custom exceptions
    - Introduce CheckoutService for cleaner business logic separation
    - Add isEmpty() method to Cart entity
    - Implement CartEmptyException and CartNotFoundException classes
    - Use @ExceptionHandler for centralized error management

--- src/main/java/com/codewithmosh/store/exceptions/CartNotFoundException.java
+++ src/main/java/com/codewithmosh/store/exceptions/CartNotFoundException.java
@@ -1,4 +1,7 @@
 package com.codewithmosh.store.exceptions;
 
 public class CartNotFoundException extends RuntimeException {
+    public CartNotFoundException() {
+        super("The cart was not found.");
+    }
 }
